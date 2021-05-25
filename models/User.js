6a8dc200611a36ef13227149a309e89d95047d70
jaeeunhyun <rosa2070@naver.com>
2021-05-25 23:43:23 +0900

    dfsfad

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10
+const jwt = require('jsonwebtoken');
 
 
 const userSchema = mongoose.Schema({
@@ -61,6 +62,24 @@ userSchema.methods.comparePassword = function(plainPassword, cb) {
     })
 }
 
+userSchema.methods.generateToken = function(cb) {
+
+    var user = this;
+
+    //jsonwebgtoken을 이용해서 token을 생성하기
+
+    var token = jwt.sign(user_id, 'secretToken')
+
+    // user._id + 'secretToken' = token
+    // ->
+    // 'secretToken' -> user._id
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 
 const User = mongoose.model('User', userSchema)
