a74b36d92afd370325df5b3b145ced3c62712f19
jaeeunhyun <rosa2070@naver.com>
2021-05-27 22:49:03 +0900

    dfsd

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
@@ -21,7 +20,11 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!~~'))
+
+app.get('/api/hello', (req, res) => {
+    res.send("안녕하세요 ~")
+})
 
 
 app.post('/api/users/register', (req, res) => {
@@ -96,5 +99,6 @@ app.get('/api/users/logout', auth, (req, res) => {
         })
 })
 
+const port = 5000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
