687eba5c8cf3a95dd36d97fe491abfba2a297ade
jaeeunhyun <rosa2070@naver.com>
2021-05-29 23:56:30 +0900

    fsdddf

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
 import LandingPage from './components/views/LandingPage/LandingPage'
 import LoginPage from './components/views/LoginPage/LoginPage'
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Auth from './hoc/auth'
 
 function App() {
   return (
@@ -24,9 +25,9 @@ function App() {
           of them to render at a time
         */}
         <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={LoginPage, false} />
+          <Route exact path="/register" component={RegisterPage, false} />
         </Switch>
       </div>
     </Router >
